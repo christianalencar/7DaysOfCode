ee2b22b48aa64bffd7b4e0fd37949ca76580a70c
Christian Alencar <100319396+christianalencar@users.noreply.github.com>
2022-08-01 14:09:55 -0400

    Add files via upload

---
+++ 7 Day Of Code - Day 2.js
@@ -0,0 +1,18 @@
+var nome = prompt("Qual o seu nome?");
+var idade = prompt("Quantos anos você tem?");
+var linguagem = prompt("Qual linguagem de programação você está estudando?");
+
+alert("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!");
+
+var resposta = prompt("Você gosta de estudar " + linguagem + "?\n\nDigite 1 para SIM ou 2 para NÃO.");
+
+while (resposta != 1 && resposta != 2) {
+    resposta = prompt("Você gosta de estudar " + linguagem + "?\n\nDigite 1 para SIM ou 2 para NÃO.");
+}
+
+if (resposta == 1) {
+        alert("Muito bom! Continue estudando e você terá muito sucesso!");
+    } else {
+     alert("Ahh que pena... Já tentou aprender outras linguagens?");
+    }
+Footer
